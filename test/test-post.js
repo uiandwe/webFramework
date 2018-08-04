81b4b13b6ff934994dc0c090b1e696713a1ad311
hyeon seung jae <uiandwe@gmail.com>
2018-08-04 14:57:03 +0900

    Update test-post.js

--- test/test-post.js
+++ test/test-post.js
@@ -8,10 +8,12 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+//필수 파라미터 포함 
 describe('/POST test', () => {
-    it('it should psot all the test', (done) => {
+    it('it should test post success', (done) => {
         chai.request(server)
             .post('/api/test')
+            .send({params: 'test'})
             .end((err, res) => {
                 res.should.have.status(201);
                 done();
@@ -19,3 +21,15 @@ describe('/POST test', () => {
     });
 });
 
+
+//파라미터가 없을시 에러
+describe('/POST test', () => {
+    it('it should test post fail', (done) => {
+        chai.request(server)
+            .post('/api/test')
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            });
+    });
+});
