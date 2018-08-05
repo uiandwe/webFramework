f1721babd39bce3ad0c2bcdb4e830d4ff8801446
hyeon seung jae <uiandwe@gmail.com>
2018-08-05 16:18:32 +0900

    Update get.js

--- server/routes/article/get.js
+++ server/routes/article/get.js
@@ -2,35 +2,17 @@ var get = {};
 var response = require("../../utils/response");
 
 
-get.validate = function () {
-    return function (req, res, next) {
-
-        req.models.test.findById(req.params.id, function(status, data){
-            if(status == 200){
-                var testData = data;
-                req.data = data;
-                next();
-            }
-            else{
-                return response.error(res, status, data);
-            }
-
-        });
-    };
+get.validate = function (req, res, next) {
+    next();
 };
 
 
-get.setParameter = function () {
-    return function (req, res, next){
-        next();
-    }
+get.setParameter = function (req, res, next) {
+    next();
 };
 
-get.supplement = function () {
-    return function (req, res, next) {
-        return response.success(res, 200, data);
-
-    };
+get.supplement = function (req, res, next) {
+    return response.success(res, 200, data);
 };
 
 
