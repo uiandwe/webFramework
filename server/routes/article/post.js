65ef69a32235976f79619be353b8246d71c9c690
hyeon seung jae <uiandwe@gmail.com>
2018-08-05 16:18:43 +0900

    Update post.js

--- server/routes/article/post.js
+++ server/routes/article/post.js
@@ -1,31 +1,23 @@
 var post = {};
 var response = require("../../utils/response");
 
-post.validate = function () {
-    return function (req, res, next) {
+post.validate = function (req, res, next) {
+    next();
+};
+
+
+post.setParameter = function (req, res, next) {
+    var body = req.body;
+    req.models.test.create(body).then(function(result) {
+        req.data = result;
         next();
-
-    };
+    }).catch(function(err) {
+        return response.error(res, 500, err);
+    });
 };
 
-
-post.setParameter = function () {
-    return function (req, res, next){
-        var body = req.body;
-        req.models.test.create(body).then(function(result) {
-            req.data = result;
-            next();
-        }).catch(function(err) {
-            return response.error(res, 500, json.parse(err));
-        });
-
-    }
-};
-
-post.supplement = function () {
-    return function (req, res, next) {
-        return response.success(res, 201, req.data);
-    };
+post.supplement = function (req, res, next) {
+    return response.success(res, 201, req.data);
 };
 
 
