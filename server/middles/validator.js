c8ff14e0676a43c517a51515304f569e8c587df5
hyeon seung jae <uiandwe@gmail.com>
2018-08-08 16:47:40 +0900

    validator 미들웨어 로직 수정

--- server/middles/validator.js
+++ server/middles/validator.js
@@ -1,52 +1,52 @@
-module.exports = function () {
-    return function (aParameterKeys, aEssentialKeys, aResetAvailableKeys) {
+module.exports = function (params) {
+    return function (req, res, next) {
 
-        return function (req, res, next) {
-            var len = 0;
-            var essencialDataCnt = 0;
-            var dataCnt = 0;
-            var data = {};
-            var bSuccess = true;
+        var aParameterKeys = params.acceptable;
+        var aEssentialKeys = params.essential;
+        var len = 0;
+        var essencialDataCnt = 0;
+        var dataCnt = 0;
+        var data = {};
+        var bSuccess = true;
 
-            var reqMethod = req.method;
-            if(reqMethod == 'POST'){
-                data = req.body;
+        var reqMethod = req.method;
+        if(reqMethod == 'POST'){
+            data = req.body;
+        }
+        else if(reqMethod == 'GET'){
+            data = req.query
+        }
+
+        for (var i = 0, len = aParameterKeys.length; i < len; ++i) {
+            if (data[aParameterKeys[i]]) {
+                essencialDataCnt++;
             }
-            else if(reqMethod == 'GET'){
-                data = req.query
-            }
+        }
 
-            for (var i = 0, len = aParameterKeys.length; i < len; ++i) {
-                if (data[aParameterKeys[i]]) {
-                    essencialDataCnt++;
-                }
-            }
+        // 요청 쿼리의 숫자가 정해진 데이터의 숫자보다 많을 때 (필요없는 쿼리문이 껴잇을경우) 예외처리.
+        if (essencialDataCnt < dataCnt) {
+            bSuccess = false;
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            return res.end(JSON.stringify({
+                code: '400_15'
+            }));
+        }
 
-            // 요청 쿼리의 숫자가 정해진 데이터의 숫자보다 많을 때 (필요없는 쿼리문이 껴잇을경우) 예외처리.
-            if (essencialDataCnt < dataCnt) {
+        // 필수 파라미터가 없을때 처리.
+        for (var i = 0; i < aEssentialKeys.length; ++i) {
+            if (!data[aEssentialKeys[i]]) {
                 bSuccess = false;
                 res.statusCode = 400;
                 res.setHeader('Content-Type', 'application/json');
                 return res.end(JSON.stringify({
                     code: '400_15'
                 }));
+                break;
             }
-
-            // 필수 파라미터가 없을때 처리.
-            for (var i = 0; i < aEssentialKeys.length; ++i) {
-                if (!data[aEssentialKeys[i]]) {
-                    bSuccess = false;
-                    res.statusCode = 400;
-                    res.setHeader('Content-Type', 'application/json');
-                    return res.end(JSON.stringify({
-                        code: '400_15'
-                    }));
-                    break;
-                }
-            }
-            if (bSuccess) {
-                next();
-            }
+        }
+        if (bSuccess) {
+            next();
         }
     }
 };
