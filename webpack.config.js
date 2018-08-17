bec20beb01256b824817b5d7bec02bac1e4edf6c
hyeon seung jae <uiandwe@gmail.com>
2018-08-17 23:28:34 +0900

    1: 웹팩 수정 (css / js 파일 분리 및 inject 추가)

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,52 @@
 const path = require('path');
 const webpack = require("webpack");
+const LatestBuildTimestamp = require('latest-build-timestamp-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 
 module.exports = {
-    entry: './app/assets/entry.js',
+    entry: {
+        app: ['./app/assets/entry.js'],
+        style: [ './app/assets/css/main.scss']
+    },
     output: {
-        path: path.join(__dirname, "/app/assets/bin/"),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
     },
     module: {
+        rules: [
+
+        ],
         loaders: [
-            { test: /\.sass$/, loader: 'style!css!sass!scss' }
+            {
+                test: /\.(scss|sass)$/i,
+                include: [
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'app/assets/css'),
+                ],
+                loaders: ["css", "sass"]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
+        new WebpackCleanupPlugin(),
+        new LatestBuildTimestamp(),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: { warnings: false }
+        // }),
+        new HtmlWebpackPlugin({
+            filename: "./index.ejs",
+            inject: 'body',
+            template: "./app/views/index.ejs"
         })
     ]
 };
